9938c11aeb955a563e0a966bbbb52c1c26813e80
HarelCohen30 <74877045+HarelCohen30@users.noreply.github.com>
2021-01-13 19:50:59 +0200

    first test

--- ex3/api/GraphNetwork.py
+++ ex3/api/GraphNetwork.py
@@ -1,15 +1,51 @@
+import json
+
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.readwrite import json_graph
-
-g = nx.Graph()
-g.add_node(5)
-g.add_edge(1, 2)
-g.add_edge(2, 3)
+from api.GraphAlgo import GraphAlgo
 
 
-print(nx.info(g))
+def __init__(self):
+    self.g = nx.Graph()
+
+def load_from_json(self, filename):
+    with open(filename) as f:
+        js_graph = json.load(f)
+    return json_graph.node_link_graph(js_graph)
+
+'''def load_from_json(self, file_name):
+    with open(file_name) as f:
+        data = json.load(f)
+    for node in data['Nodes']:
+        if 'pos' in node:
+            p = node['pos']
+            pos_list = p.split(',')
+            pos_float = [float(x) for x in pos_list]
+            tup_pos = (pos_float[0], pos_float[1], pos_float[2])
+            self.g.add_node(node['id'], pos=tup_pos)
+        else:
+            self.g.add_node(node['id'])
+        # self.gr.vertices[self.gr.nodeCounter] = node_data.NodeData(node['id'])
+        # self.gr.nodeCounter += 1
+    for edge in data['Edges']:
+        src = edge['src']
+        w = edge['w']
+        dest = edge['dest']
+        self.g.add_edge(src, dest, weight=w)'''
+
+
+file = '../data/A5'
+load_from_json(file)
+
+
+'''g = nx.Graph()
+    g.add_node(5)
+    g.add_edge(1, 2)
+    g.add_edge(2, 3)'''
+
+'''print(nx.info(g))
 nx.draw(g)  # draw graph without numbering
 nx.draw(g, with_labels=1)  # draw graph with numbering
 nx.draw(g, with_labels=1, node_color='b')  # draw graph with numbering, node color: blue
-plt.show()
+plt.show()'''
--- ex3/api/testDiGraph.py
+++ ex3/api/testDiGraph.py
@@ -1,23 +1,51 @@
-from api import DiGraph
+from api.DiGraph import DiGraph
+
+import unittest
 
 
-def build_graph(self):
-    g: DiGraph
-    for x in range(6):
-        g.add_node(x)
-    g.add_edge(0, 1, 5)
-    g.add_edge(1, 0, 8)
-    g.add_edge(0, 2, 2)
-    g.add_edge(2, 0, 1)
-    g.add_edge(2, 3, 1)
-    g.add_edge(3, 2, 0.5)
-    g.add_edge(3, 4, 12)
-    g.add_edge(4, 3, 1)
-    g.add_edge(0, 4, 20)
-    g.add_edge(4, 0, 20)
-    g.add_edge(4, 1, 5)
-    g.add_edge(1, 4, 5)
-    return g
+class TestDiGraph (unittest.TestCase):
+
+    def __init__(self):
+        self.g = DiGraph()
+
+    def build_graph(self):
+        for x in range(5):
+            self.g.add_node(x)
+        self.g.add_edge(0, 1, 5)
+        self.g.add_edge(1, 0, 8)
+        self.g.add_edge(0, 2, 2)
+        self.g.add_edge(2, 0, 1)
+        self.g.add_edge(2, 3, 1)
+        self.g.add_edge(3, 2, 0.5)
+        self.g.add_edge(3, 4, 12)
+        self.g.add_edge(4, 3, 1)
+        self.g.add_edge(0, 4, 20)
+        self.g.add_edge(4, 0, 20)
+        self.g.add_edge(4, 1, 5)
+        self.g.add_edge(1, 4, 5)
+
+    def test_1(self):
+        self.build_graph()
+        self.assertEqual(self.g.nodeCounter, 5)
+
+    '''def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)'''
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
 
 
 
