f0fadc77170c1aa759e74aae2764a70e81705449
HarelCohen30 <74877045+HarelCohen30@users.noreply.github.com>
2021-01-13 21:19:04 +0200

    first test

--- ex3/api/GraphNetwork.py
+++ ex3/api/GraphNetwork.py
@@ -6,39 +6,42 @@ from networkx.readwrite import json_graph
 from api.GraphAlgo import GraphAlgo
 
 
-def __init__(self):
-    self.g = nx.Graph()
+# class GraphNetwork:
 
-def load_from_json(self, filename):
-    with open(filename) as f:
-        js_graph = json.load(f)
-    return json_graph.node_link_graph(js_graph)
+#    def __init__(self):
+#       self.g = nx.Graph()
+# def load_from_json(self, filename):\
 
-'''def load_from_json(self, file_name):
-    with open(file_name) as f:
-        data = json.load(f)
-    for node in data['Nodes']:
-        if 'pos' in node:
-            p = node['pos']
-            pos_list = p.split(',')
-            pos_float = [float(x) for x in pos_list]
-            tup_pos = (pos_float[0], pos_float[1], pos_float[2])
-            self.g.add_node(node['id'], pos=tup_pos)
-        else:
-            self.g.add_node(node['id'])
-        # self.gr.vertices[self.gr.nodeCounter] = node_data.NodeData(node['id'])
-        # self.gr.nodeCounter += 1
-    for edge in data['Edges']:
-        src = edge['src']
-        w = edge['w']
-        dest = edge['dest']
-        self.g.add_edge(src, dest, weight=w)'''
+file = "../data/A5"
+'''with open(file) as f:
+    js_graph = json.load(f)
+    g = json_graph.node_link_graph(js_graph)'''
 
+g=nx.Graph()
 
-file = '../data/A5'
-load_from_json(file)
-
-
+with open(file) as f:
+    data = json.load(f)
+for node in data['Nodes']:
+    if 'pos' in node:
+        p = node['pos']
+        pos_list = p.split(',')
+        pos_float = [float(x) for x in pos_list]
+        tup_pos = (pos_float[0], pos_float[1], pos_float[2])
+        g.add_node(node['id'], pos=tup_pos)
+    else:
+        g.add_node(node['id'])
+    # self.gr.vertices[self.gr.nodeCounter] = node_data.NodeData(node['id'])
+    # self.gr.nodeCounter += 1
+for edge in data['Edges']:
+    src = edge['src']
+    w = edge['w']
+    dest = edge['dest']
+    g.add_edge(src, dest, weight=w)
+print (type(g))
+nx.draw(g)
+plt.show()
+print(nx.connected_components(g))
+print(nx.shortest_path(g))
 '''g = nx.Graph()
     g.add_node(5)
     g.add_edge(1, 2)
